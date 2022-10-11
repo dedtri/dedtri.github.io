332db5e9cbfd7629b062d93915e13b52ec7188fe
Martin The Khang Nguyen <mart72w3@pc-tec.local>
2022-10-11 13:18:56 +0200

    a

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,13 +1,47 @@
 import React from 'react'
 import './contact.css'
 import {AiOutlineMail} from 'react-icons/ai'
+import {FaDiscord} from 'react-icons/fa'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
+import {useState} from 'react';
+import { useTransition } from 'react';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_q317hoi', 'template_htzppwp', form.current, '-B9gBTZeH3HNjK-CS')
+
+    .then((result) => {
+      setIsAlertVisible(true);
+      setTimeout(() => {
+       setIsAlertVisible(false);
+   }, 3500);
+   
+  }, (error) => {
+      console.log(error.text);
+  });
+
+    e.target.reset()
+  };
+
+  const [ isAlertVisible, setIsAlertVisible ] = React.useState(false);
+
+  // const handleButtonClick = () => {
+  //     setIsAlertVisible(true);
+
+  //          setTimeout(() => {
+  //           setIsAlertVisible(false);
+  //       }, 3000);
+  // }
+
   return (
     <section id='contact'>
       
-      <h5>get in touch</h5>
-      <h2>contact me</h2>
+      <h2>Contact me</h2>
 
       <div className="container contact__container">
         <div className="contact__mail">
@@ -17,19 +51,30 @@ const Contact = () => {
 <h5>jaackie@live.dk</h5>
 <a href="mailto:jaackie@live.dk" target="_blank">Send an email</a>
           </article>
+          <article className='mail'>
+<FaDiscord className='mail__icon'/>
+<h4>Discord</h4>
+<h5>Trie#0123</h5>
+<a href="https://discordapp.com/users/145968715022729216" target="_blank">Let's connect</a>
+          </article>
         </div>
 
-        <form action="" className='contact__form'>
-        <input type="text" name='name' placeholder='Name' required className='form__name'/>
+        <form ref={form} onSubmit={sendEmail} className='contact__form'>
+        <div>
+        <input type="text" name='name' placeholder='Name' required className='form__name'/> 
         <input type="email" name='email' placeholder='Email' required className='form__name'/>
-        <input type="text" name='subject' placeholder='Subject' required />
+        </div>
+        <input type="text" name='subject' placeholder='Subject' required/>
         <textarea name='message' rows="7" placeholder='Message' required></textarea>
-        <button type='submit' className='btn btn-primary'>Send Message!</button>
+        <button type='submit' className='' id='submit__button' onClick="">Send Message!</button>
         </form>
 
+   {isAlertVisible &&  
+  <div className='mailsent__box'>Your email has been sent!</div> 
+                } 
 
-
-      </div>
+      
+      </div>    
 
     </section>
   )
--- src/index.js
+++ src/index.js
@@ -3,3 +3,21 @@ import App from './App'
 import './index.css'
 
 ReactDOM.render(<App/>, document.querySelector("#root"))
+
+const btn = document.getElementById('submit__button');
+
+btn.addEventListener('click', function handleClick() {
+    btn.textContent = 'Sending Message...';
+    btn.style.opacity = '0.3';
+            setTimeout(() => {
+                btn.style.opacity = '1';
+                btn.textContent = 'Send Message!';
+         }, 4500);
+  });
+
+// btn.addEventListener('mouseleave', (event) => {
+//     btn.textContent = 'Send Message!';
+// });
+
+
+
