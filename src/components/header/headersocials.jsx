a5e2dab505f298fc9a300fbe56d16ca4590c08ce
Martin The Khang Nguyen <mart72w3@pc-tec.local>
2022-10-11 13:53:01 +0200

    a

--- src/components/header/headersocials.jsx
+++ src/components/header/headersocials.jsx
@@ -6,9 +6,9 @@ import {BsYoutube} from 'react-icons/bs'
 const headersocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://github.com" target="_blank"><BsGithub/></a>
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://youtube.com" target="_blank"><BsYoutube/></a>
+        <a href="https://github.com/dedtri" target="_blank"><BsGithub/></a>
+        <a href="https://www.linkedin.com/in/martin-nguyen-805ab4225/" target="_blank"><BsLinkedin/></a>
+        <a href="https://www.youtube.com/channel/UCwwlViVNzX4AQ3y3IKWZhmQ" target="_blank"><BsYoutube/></a>
     </div>
   )
 }
